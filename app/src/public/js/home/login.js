d02362cc6c343e5cbb27668b6681d17d974ddff4
zokjebi <myimgman@gmail.com>
2022-08-13 14:50:01 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -24,4 +24,13 @@ function login() {
     };
 
     console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login", {
+        method: "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(req)
+    });
+
 }
