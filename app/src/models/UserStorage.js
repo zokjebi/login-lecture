76cddb79ecaf31b6c0436cbf5f7d43da732257f6
zokjebi <myimgman@gmail.com>
2022-08-16 15:32:24 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -21,6 +21,17 @@ class UserStorage {
         //console.log(newUsers);
         return newUsers;
     }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,8 @@
 // ECMA 스크립트 준수
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+//const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -14,37 +15,10 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        //console.log(req.body);
-        const id = req.body.id,
-              password = req.body.password;
-
-        /*
-        const userStorage = new UserStorage();
-        console.log(userStorage.users);
-        */
-
-        //console.log(UserStorage.getUsers("id", "password"));
-        //console.log(UserStorage.getUsers("password"));
-
-        const users = UserStorage.getUsers("id", "password");
-
-        
-        const response = {};
-
-        
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        
-
-        response.success = false;
-        response.msg = "로그인에 실패하였습니다.";
+        const user = new User(req.body);
+        const response = user.login();
+        //console.log(response);
         return res.json(response);
-        
     }, 
 };
 
